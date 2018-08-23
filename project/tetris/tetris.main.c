c94b9a7424b0c30cf484e2577a1453f6c1315e00
DESKTOP-187MMR2\kfru1 <ajun@mtellis.org>
2018-08-22 21:04:45 -0600

    files

--- project/tetris/tetris.main.c
+++ project/tetris/tetris.main.c
@@ -16,6 +16,38 @@
 #define flase 0
 #define ture 1
 
-#define ACTIVE_BLOCK -2 // 
-#define CELLING -1 //
-#define EMPTY 0 // 
+#define ACTIVE_BLOCK -2 // on borad of block 
+#define CELLING -1 // if block can move it's 0 or postive number
+#define WALL 1
+#define INACTIVE_BLOCK 2 // value of completed moved block
+#define EMPTY 0 // if block can move it's negative number
+
+#define MAIN_X 11 // width of game board
+#define MAIN_Y 23 // length of game board
+#define MAIN_X_ADJ 3 // set of width
+#define MAIN_Y_ADJ 1 // set of length
+
+#define STATUS_X_ADJ MAIN_X_ADJ+MAIN_X+1 // 
+
+int STATUS_Y_LEVEL; // location Y of LEVEL
+int STATUS_Y_SCORE; // location Y of SCORE
+int STATUS_Y_GOAL; // location Y of GOAL
+
+int blocks[7][4][4][4]={
+	{{0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0},{0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0},
+		 {0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0},{0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0}},
+	{{0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0},{0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0},
+		 {0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0},{0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0}},
+	{{0,0,0,0,1,1,0,0,0,1,1,0,0,0,0,0},{0,0,0,0,0,0,1,0,0,1,1,0,0,1,0,0},
+		 {0,0,0,0,1,1,0,0,0,1,1,0,0,0,0,0},{0,0,0,0,0,0,1,0,0,1,1,0,0,1,0,0}},
+	{{0,0,0,0,0,1,1,0,1,1,0,0,0,0,0,0},{0,0,0,0,1,0,0,0,1,1,0,0,0,1,0,0},
+		 {0,0,0,0,0,1,1,0,1,1,0,0,0,0,0,0},{0,0,0,0,1,0,0,0,1,1,0,0,0,1,0,0}},
+	{{0,0,0,0,0,0,1,0,1,1,1,0,0,0,0,0},{0,0,0,0,1,1,0,0,0,1,0,0,0,1,0,0},
+		 {0,0,0,0,0,0,0,0,1,1,1,0,1,0,0,0},{0,0,0,0,0,1,0,0,0,1,0,0,0,1,1,0}},
+	{{0,0,0,0,1,0,0,0,1,1,1,0,0,0,0,0},{0,0,0,0,0,1,0,0,0,1,0,0,1,1,0,0},
+		 {0,0,0,0,0,0,0,0,1,1,1,0,0,0,1,0},{0,0,0,0,0,1,1,0,0,1,0,0,0,1,0,0}},
+	{{0,0,0,0,0,1,0,0,1,1,1,0,0,0,0,0},{0,0,0,0,0,1,0,0,0,1,1,0,0,1,0,0},
+		 {0,0,0,0,0,0,0,0,1,1,1,0,0,1,0,0},{0,0,0,0,0,1,0,0,1,1,0,0,0,1,0,0}}
+	}; // shape of block 4x4, blocks[b_type][b_rotation][i][j] 
+
+int b_type; //
