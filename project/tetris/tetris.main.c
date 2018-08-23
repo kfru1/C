5bfe2ff80ca96ac7b7cb720a491f05677b2db7f8
DESKTOP-187MMR2\kfru1 <ajun@mtellis.org>
2018-08-22 20:49:58 -0600

    files

---
+++ project/tetris/tetris.main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<windows.h>
+#include<conio.h>
+#include<time.h>
+#include<stdlib.h>
+
+#define UP 72 // spin
+#define LEFT 75 // move toward left
+#define RIGHT 77 // move toward right
+#define DOWN 80 // softdrop
+#define SPACE 32 // hard drop
+#define p 112 // pause
+#define P 80 // pause
+#define ESC 27 // stop game
+
+#define flase 0
+#define ture 1
+
+#define ACTIVE_BLOCK -2 // 
+#define CELLING -1 //
+#define EMPTY 0 // 
